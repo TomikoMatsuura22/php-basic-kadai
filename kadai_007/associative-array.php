30d01f9647eaa1381961e96241f67f529a3fc650
TomikoMatsuura <tomiko2016@icloud.com>
2025-05-22 13:48:06 +0900

    kadai_007①アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 連想配列に値を出力する
+    $user_names = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 連想配列の値を出力する
+    print_r($user_names);
+    ?>
+  </p>
+
+
+  
+</body>
+</html>
